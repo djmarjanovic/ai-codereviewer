670904b74a191615025f1e05c94d320ae7bffed9
djordje <dorde.marjanovic@presight.ai>
2024-09-02 14:10:30 +0400

    add openai base url

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import parseDiff, { Chunk, File } from "parse-diff";
 import minimatch from "minimatch";
 
 const GITHUB_TOKEN: string = core.getInput("GITHUB_TOKEN");
+const OPENAI_API_BASE_URL: string = core.getInput("OPENAI_API_BASE_URL");
 const OPENAI_API_KEY: string = core.getInput("OPENAI_API_KEY");
 const OPENAI_API_MODEL: string = core.getInput("OPENAI_API_MODEL");
 
@@ -13,6 +14,7 @@ const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
+  baseURL: OPENAI_API_BASE_URL
 });
 
 interface PRDetails {
